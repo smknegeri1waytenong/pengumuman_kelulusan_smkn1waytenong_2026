d21a84a76f18cdb0cf81217155a01577af53b6c4
smknegeri1waytenong <smknegeri1waytenong@gmail.com>
2026-05-03 22:32:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ATUR TANGGAL DISINI
-const targetDate = new Date("May 4, 2026 17:00:00").getTime();
+const targetDate = new Date("May 3, 2026 22:32:00").getTime();
 
 const updateTimer = () => {
     const now = new Date().getTime();
